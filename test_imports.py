7e52c7960efd4fef48ce3e22dc8da1411129c97b
jenniferluu6 <jennifer.luu6@gmail.com>
2026-04-02 14:22:57 -0400

    add link

---
+++ test_imports.py
@@ -0,0 +1,12 @@
+import torch
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("PyTorch version:", torch.__version__)
+print("Pandas version:", pd.__version__)
+print("GeoPandas version:", gpd.__version__)
+print("NumPy version:", np.__version__)
+print("Matplotlib version:", plt.matplotlib.__version__)
+print("\n✓ All imports successful!")
